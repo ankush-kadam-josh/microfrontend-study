a81b7bc7d2c9e321bdb1a5b39f434127c835619e
Ankush Kadam <ankush.kadam@druva.com>
2023-02-10 12:41:09 +0530

    improved comment

--- container/src/index.js
+++ container/src/index.js
@@ -1,6 +1,6 @@
 /**
  * This pattern allows webpack to identify that before loading the project into browser,
- * we first need to check for resources required, (faker shared resource in this case)
+ * we first need to check for resources required, (faker shared resource in this case(see webpack file of products and cart projects))
  * and it will load all the dependencies first asynchronously and then loads up the project into browser
  * 
 */
